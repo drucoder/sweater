831af1c24091f3e04284b8bdae7565d95eda9ff2
Andrey <i@dru.su>
2018-03-31 01:08:58 +0700

    Initial commit

---
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -0,0 +1,26 @@
+package com.example.sweater;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            Map<String, Object> model
+    ) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model) {
+        model.put("some", "hello, letsCode!");
+        return "main";
+    }
+}
